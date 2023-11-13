e14366211c26f761cfd1f5c53aa9ea28814d2950
Ralph Schindler <ralph@ralphschindler.com>
2023-11-12 21:39:51 -0600

    Various refactors

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -13,6 +13,8 @@ import css from 'highlight.js/lib/languages/css';
 import yaml from 'highlight.js/lib/languages/yaml';
 import plaintext from 'highlight.js/lib/languages/plaintext';
 
+import '../css/app.css';
+
 // Then register the languages you need
 
 window.Alpine        = Alpine;
@@ -24,27 +26,20 @@ Alpine.plugin(persist);
 Alpine.store('darkMode', {
     on: Alpine.$persist(false).as('darkMode'),
 
-    init() {
-        if (this.on) {
-            document.querySelector('a.dark-icon > svg:first-child').style.display = 'none';
-            document.querySelector('a.dark-icon > svg:last-child').style.display = 'block';
-            document.querySelector('a.home').style.backgroundImage = 'url(/assets/img/pop-php-logo-white2.png)';
-            Alpine.$persist({ value: true }).as('darkMode')
-        } else {
-            document.querySelector('a.dark-icon > svg:first-child').style.display = 'block';
-            document.querySelector('a.dark-icon > svg:last-child').style.display = 'none';
-            document.querySelector('a.home').style.backgroundImage = 'url(/assets/img/pop-php-logo2.png)';
-            Alpine.$persist({ value: false }).as('darkMode')
-        }
-    },
-
     toggle() {
         this.on = ! this.on;
-        this.init();
+        this.on
+            ? document.querySelector('body').classList.add('dark')
+            : document.querySelector('body').classList.remove('dark')
     }
 })
+
 Alpine.start();
-Alpine.store('darkMode').init();
+
+// set initial page state after store is initialized
+if (Alpine.store('darkMode').on === true) {
+    document.querySelector('body').classList.add('dark')
+}
 
 hljs.registerLanguage('php', php);
 hljs.registerLanguage('php-template', phpTemplate);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: 'production',
-    entry: ['./app/resources/js/app.js'],
+    mode: 'development',
+    entry: [
+        './app/resources/js/app.js',
+        './app/resources/css/app.css'
+    ],
     output: {
-        path: path.resolve(__dirname, './public/assets/js'),
-        filename: 'app.min.js'
+        path: path.resolve(__dirname, './public/assets/'),
+        filename: 'app.js',
     },
-    optimization: { minimize: true },
-    watch: true
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename:"app.css",
+    })]
 };
