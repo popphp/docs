7ac93079a9f42b60e60a848edb9df07cd65c182c
Nick Sagona <nick@nolainteractive.com>
2023-11-26 17:26:07 -0600

    Update conf

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,40 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = (env) => {
-    return {
-        mode: 'development',
-        entry: [
-            './app/resources/js/app.js',
-            './app/resources/css/app.css'
-        ],
-        optimization: {
-            minimize: env.prod,
-            minimizer: [
-                new CssMinimizerPlugin(),
-                new TerserPlugin({
-                    extractComments: false,
-                    terserOptions: {
-                        format: {
-                            comments: false,
-                        },
-                    },
-                }),
-            ],
-        },
-        output: {
-            path: path.resolve(__dirname, './public/assets/'),
-            filename: 'app.js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-                }
-            ]
-        },
-        plugins: [new MiniCssExtractPlugin({
-            filename: 'app.css'
-        })]
-    }
+  return {
+    mode: 'development',
+    entry: [
+      './app/resources/js/app.js',
+      './app/resources/css/app.css'
+    ],
+    optimization: {
+      minimize: env.prod,
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+          extractComments: false,
+          terserOptions: {
+            format: {
+              comments: false,
+            },
+          },
+        }),
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, './public/assets/'),
+      filename: 'app.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        }
+      ]
+    },
+    plugins: [new MiniCssExtractPlugin({
+      filename: 'app.css'
+    })]
+  }
 }
