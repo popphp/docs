6f71dd05c7e29584fef35f887d15bf0d790d2804
Nick Sagona <nick@noladev.com>
2026-08-26 14:52:44 -0500

    Force version

--- app/config/app.http.php
+++ app/config/app.http.php
@@ -2,16 +2,14 @@
 
 return [
     'routes' => [
-        '/6.x' => [
-            '/search' => [
-                'controller' => 'Pop\Docs\Http\Controller\IndexController',
-                'action'     => 'search'
-            ],
-            '*' => [
-                'controller' => 'Pop\Docs\Http\Controller\IndexController',
-                'action'     => 'route',
-                'default'    => true
-            ]
+        '/6.x/search' => [
+            'controller' => 'Pop\Docs\Http\Controller\IndexController',
+            'action'     => 'search'
+        ],
+        '*' => [
+            'controller' => 'Pop\Docs\Http\Controller\IndexController',
+            'action'     => 'route',
+            'default'    => true
         ]
     ],
     'database' => include __DIR__ . '/database.php',
